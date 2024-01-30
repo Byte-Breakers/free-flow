e7c87b83136c3347e3f9ad8c31dfa57d24eed0a3
Amrith R Naik <amrithrnaik@gmail.com>
2024-01-31 00:48:25 +0530

    Formatted with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,37 @@ import { FiSearch } from "react-icons/fi";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen relative">
-      <div className="w-full h-full bg-[#EEE7FE] flex items-center justify-around">
+    <main className="relative h-screen w-screen">
+      <div className="flex h-full w-full items-center justify-around bg-[#EEE7FE]">
         {/* Card */}
-        <div className="w-[90%] h-[80%] bg-gradient-to-br from-[#A57EF7] to-[#6265D7] rounded-2xl shadow-xl px-[10%] py-[5%] flex justify-around">
+        <div className="flex h-[80%] w-[90%] justify-around rounded-2xl bg-gradient-to-br from-[#A57EF7] to-[#6265D7] px-[10%] py-[5%] shadow-xl">
           {/* Hero Section */}
-          <div className="w-[40%] py-[12%] h-full flex flex-col justify-between">
+          <div className="flex  h-full w-[40%] flex-col justify-between py-[12%]">
             {/* AI Search Heading */}
-            <h1 className="font-extrabold text-white drop-shadow-md text-5xl">AI Search</h1>
+            <h1 className="text-5xl font-extrabold text-white drop-shadow-md">
+              AI Search
+            </h1>
             {/* Description */}
-            <p className="text-white font-medium opacity-70 "> Your Gateway to Infinite Knowledge. Discover Free Courses, Navigate, and Dive into Wisdom with Seamless Search!</p>
+            <p className="font-medium text-white opacity-70 ">
+              {" "}
+              Your Gateway to Infinite Knowledge. Discover Free Courses,
+              Navigate, and Dive into Wisdom with Seamless Search!
+            </p>
             {/* Search bar and Button */}
             <form action="" className="flex h-8">
-              <input type="search" className="opacity-35 rounded-md px-3 text-sm shadow-inner shadow-neutral-300" placeholder="Search for a course" />
+              <input
+                type="search"
+                className="rounded-md px-3 text-sm opacity-35 shadow-inner shadow-neutral-300"
+                placeholder="Search for a course"
+              />
               <div className="w-[2%]" />
-              <button className="bg-white text-sm px-3 rounded-md text-[#6265D7] font-medium">Search</button>
+              <button className="rounded-md bg-white px-3 text-sm font-medium text-[#6265D7]">
+                Search
+              </button>
             </form>
           </div>
           {/* Illustration */}
-          <div className="w-1/2 h-full relative">
+          <div className="relative h-full w-1/2">
             <Image fill src="/mainIllustration.svg" alt="logo" />
           </div>
         </div>
