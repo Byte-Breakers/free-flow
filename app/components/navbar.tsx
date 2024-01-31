a2cc971560fc27e699a1322df021f729c90d643c
Amrith R Naik <amrithrnaik@gmail.com>
2024-01-31 22:15:48 +0530

    Navbar Design complete

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className='fixed h-12 w-full bg-white bg-opacity-10 backdrop-filter backdrop-blur-md border-b-2'>
-      
+    <div className='px-4 h-14 w-full bg-[#EEE7FE] border-b-2 border-[#6265D7] border-opacity-10 flex justify-between items-center'>
+      {/* Logo & Name */}
+      <div className='flex items-center h-full'>
+      <Image width={50} height={50}  src="/logo.svg" alt="Logo" />
+        <h1 className='font-bold ml-2 text-xl text-[#6265D7]'>Free</h1>
+        <h1 className='font-bold text-xl text-[#F88F4F]'>Flow</h1>
+      </div>
+      {/* Login Button or Profile Button */}
+      {/* !CODE TO BE EDITED */}
+      <button className='bg-[#6265D7] text-sm h-9 px-4 py-2 text-white font-medium flex items-center rounded-md'>Login</button>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,23 @@ export default function Home() {
   return (
     <main className="relative h-screen w-screen">
       <Navbar />
-      <div className="flex h-full w-full items-center justify-around bg-[#EEE7FE]">
+      <div className="flex h-[92dvh] w-full items-center justify-around bg-[#EEE7FE]">
         {/* Card */}
         <div className="flex h-[80%] w-[90%] justify-around rounded-2xl bg-gradient-to-br from-[#A57EF7] to-[#6265D7] px-[10%] py-[5%] shadow-xl">
           {/* Hero Section */}
-          <div className="flex  h-full w-[40%] flex-col justify-between py-[12%]">
+          <div className="flex  h-full w-[40%] flex-col justify-center">
             {/* AI Search Heading */}
             <h1 className="text-5xl font-extrabold text-white drop-shadow-md">
               AI Search
             </h1>
             {/* Description */}
-            <p className="font-medium text-white opacity-70 ">
+            <p className="mt-3 font-medium text-white opacity-70 ">
               {" "}
               Your Gateway to Infinite Knowledge. Discover Free Courses,
               Navigate, and Dive into Wisdom with Seamless Search!
             </p>
             {/* Search bar and Button */}
-            <form action="" className="flex h-8">
+            <form action="" className="flex h-8 mt-3">
               <input
                 type="search"
                 className="rounded-md px-3 text-sm opacity-35 shadow-inner shadow-neutral-300"
@@ -30,8 +30,7 @@ export default function Home() {
               />
               <div className="w-[2%]" />
               <button className="rounded-md bg-white px-3 text-sm font-medium text-[#6265D7]">
-                Search
-              </button>
+                Search</button>
             </form>
           </div>
           {/* Illustration */}
