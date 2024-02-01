be049a614a133bb85e3e3706642ab2d146779b4f
Amrith R Naik <amrithrnaik@gmail.com>
2024-02-01 19:54:13 +0530

    Login Button Working

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className='px-4 h-14 w-full bg-[#EEE7FE] border-b-2 border-[#6265D7] border-opacity-10 flex justify-between items-center'>
       {/* Logo & Name */}
-      <div className='flex items-center h-full'>
+      <Link href={'/'} className='flex items-center h-full'>
       <Image width={50} height={50}  src="/logo.svg" alt="Logo" />
         <h1 className='font-bold ml-2 text-xl text-[#6265D7]'>Free</h1>
         <h1 className='font-bold text-xl text-[#F88F4F]'>Flow</h1>
-      </div>
+      </Link>
       {/* Login Button or Profile Button */}
       {/* !CODE TO BE EDITED */}
-      <button className='bg-[#6265D7] text-sm h-9 px-4 py-2 text-white font-medium flex items-center rounded-md' >Login</button>
+      <Link href={'/login'} className='bg-[#6265D7] text-sm h-9 px-4 py-2 text-white font-medium flex items-center rounded-md'>Login</Link>
     </div>
   )
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const LoginPage = () => {
   return (
     <>
-    {/* Navbar Section */}
+      {/* Navbar Section */}
       <div className='px-4 h-14 w-full bg-[#EEE7FE] border-b-2 border-[#6265D7] border-opacity-10 flex justify-between items-center'>
-      {/* Logo & Name */}
-      <div className='flex items-center h-full'>
-      <Image width={50} height={50}  src="/logo.svg" alt="Logo" />
-        <h1 className='font-bold ml-2 text-xl text-[#6265D7]'>Free</h1>
-        <h1 className='font-bold text-xl text-[#F88F4F]'>Flow</h1>
+        {/* Logo & Name */}
+        <Link href={'/'} className='flex items-center h-full'>
+          <Image width={50} height={50} src="/logo.svg" alt="Logo" />
+          <h1 className='font-bold ml-2 text-xl text-[#6265D7]'>Free</h1>
+          <h1 className='font-bold text-xl text-[#F88F4F]'>Flow</h1>
+        </Link>
       </div>
-      {/* Login Button or Profile Button */}
-      {/* !CODE TO BE EDITED */}
-      <button className='hidden bg-[#6265D7] text-sm h-9 px-4 py-2 text-white font-medium items-center rounded-md' >Login</button>
-    </div>
-    {/* Login Section */}
-    <div className='h-[92vh] bg-[#EEE7FE] flex flex-col items-center justify-center'>
-      <h1 className='font-semibold text-xl'>Login via Google</h1>
-      <button className='w-24 mt-2 py-1 bg-[#6265D7] rounded-md text-white'> Login </button>
-      <div className='h-8'/>
-      <h1 className='font-semibold text-xl'>Create an account</h1>
-      <button className='w-24 mt-2 rounded-md border-2 border-[#6265D7] border-opacity-50 py-1 bg-white'> Google </button>
-    </div>
+      {/* Login Section */}
+      <div className='h-[92vh] bg-[#EEE7FE] flex flex-col items-center justify-center'>
+        <h1 className='font-semibold text-xl'>Login via Google</h1>
+        <button className='w-24 mt-2 py-1 bg-[#6265D7] rounded-md text-white'> Login </button>
+        <div className='h-8' />
+        <h1 className='font-semibold text-xl'>Create an account</h1>
+        <button className='w-24 mt-2 rounded-md border-2 border-[#6265D7] border-opacity-50 py-1 bg-white'> Google </button>
+      </div>
     </>
   )
 }
