a06a4b83762d4b6617ac703cdcc64e80e7972121
Nandan Upadhyaya <nandanupadhyaya18@gmail.com>
2024-02-03 20:57:16 +0530

    Added magnifying icon and transition to LOGIN WITH GOOGLE button

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 
 const LoginPage = () => {
   return (
     <>
       {/* Navbar Section */}
-      <div className='px-4 h-14 w-full bg-[#EEE7FE] border-b-2 border-[#6265D7] border-opacity-10 flex justify-between items-center'>
+      <div className='px-4 h-14 w-full bg-[grey] border-b-2 border-[#6265D7] border-opacity-10 flex justify-between items-center'>
         {/* Logo & Name */}
         <Link href={'/'} className='flex items-center h-full'>
           <Image width={50} height={50} src="/logo.svg" alt="Logo" />
@@ -15,13 +16,16 @@ const LoginPage = () => {
         </Link>
       </div>
       {/* Login Section */}
-      <div className='h-[92vh] bg-[#EEE7FE] flex flex-col items-center justify-center'>
-        <h1 className='font-semibold text-xl'>Login via Google</h1>
-        <button className='w-24 mt-2 py-1 bg-[#6265D7] rounded-md text-white'> Login </button>
-       
+      <div className='h-[92vh] bg-[#EEE7FE] flex flex-col items-center justify-center '>
+        <h1 className='font-semibold text-xl'>
+          Login via Google
+        </h1>
+        <button className='w-24 mt-2 py-1 bg-[#6265D7] rounded-md text-white transition duration-300 ease-in-out hover:bg-blue-600 transform hover:-translate-y-0.5 hover:scale-105'>
+          Login
+        </button>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
